2e8c7fb6d2f07225feedf3bc6d360f58c416c6ea
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-02-21 23:58:47 +0800

    Update 27_Remove_Element.c

--- 27_Remove_Element.c
+++ 27_Remove_Element.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-typedef struct {
-    int nums[8];
-    int numsSize, val;
-} Example_t;
-
 int removeElement(int *nums, int numsSize, int val) {
     int j = 0;
     for (int i = 0; i < numsSize; i++) if (nums[i] != val) {
@@ -25,16 +20,19 @@ void printArray(const int *nums, int numsSize, const char *name) {
 }
 
 int main() {
-    Example_t ex[] = {
+    struct TestCase {
+        int nums[8];
+        int numsSize, val;
+    } tc[] = {
         {.nums = {3,2,2,3}, .numsSize = 4, .val = 3},
         {.nums = {0,1,2,2,3,0,4,2}, .numsSize = 8, .val = 2}};
 
-    for (int i = 0; i < sizeof(ex) / sizeof(ex[0]); i++) {
+    for (int i = 0; i < sizeof(tc) / sizeof(tc[0]); i++) {
         printf("Example %d:\n", i + 1);
-        printArray(ex[i].nums, ex[i].numsSize, "Input: nums");
-        printf(", val = %d\n", ex[i].val);
-        printf("Output: %d, ", removeElement(ex[i].nums, ex[i].numsSize, ex[i].val));
-        printArray(ex[i].nums, ex[i].numsSize, "nums");
+        printArray(tc[i].nums, tc[i].numsSize, "Input: nums");
+        printf(", val = %d\n", tc[i].val);
+        printf("Output: %d, ", removeElement(tc[i].nums, tc[i].numsSize, tc[i].val));
+        printArray(tc[i].nums, tc[i].numsSize, "nums");
         printf("\n\n");
     }
 
