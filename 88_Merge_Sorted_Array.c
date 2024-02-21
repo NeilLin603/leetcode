3ca21a1b42274eda74a3421e3e1798bfa878f85c
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-02-22 00:01:41 +0800

    Update 88_Merge_Sorted_Array.c

--- 88_Merge_Sorted_Array.c
+++ 88_Merge_Sorted_Array.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 
-typedef struct {
-    int nums1[6];
-    int nums2[6];
-    int m, n;
-} Example_t;
-
 void merge(int *nums1, int nums1Size, int m, int *nums2, int nums2Size, int n) {
     while (m && n) {
         nums1[m + n] = nums1[m - 1] > nums2[n - 1] ? nums1[--m] : nums2[--n];
@@ -27,19 +21,23 @@ void printArray(const int *nums, int numsSize, const char *name) {
 }
 
 int main() {
-    Example_t ex[] = {
+    struct TestCase {
+        int nums1[6];
+        int nums2[6];
+        int m, n;
+    } tc[] = {
         {.nums1 = {1,2,3,0,0,0}, .m = 3, .nums2 = {2,5,6}, .n = 3},
         {.nums1 = {1}, .m = 1, .n = 0},
         {.nums1 = {0}, .m = 0, .nums2 = {1}, .n = 1}};
 
-    for (int i = 0; i < sizeof(ex) / sizeof(ex[0]); i++) {
+    for (int i = 0; i < sizeof(tc) / sizeof(tc[0]); i++) {
         printf("Example %d:\n", i + 1);
-        printArray(ex[i].nums1, ex[i].m + ex[i].n, "Input: nums1");
-        printf(", m = %d, ", ex[i].m);
-        printArray(ex[i].nums2, ex[i].n, "nums2");
-        printf(", n = %d\n", ex[i].n);
-        merge(ex[i].nums1, 0, ex[i].m, ex[i].nums2, 0, ex[i].n);
-        printArray(ex[i].nums1, ex[i].m + ex[i].n, "Output");
+        printArray(tc[i].nums1, tc[i].m + tc[i].n, "Input: nums1");
+        printf(", m = %d, ", tc[i].m);
+        printArray(tc[i].nums2, tc[i].n, "nums2");
+        printf(", n = %d\n", tc[i].n);
+        merge(tc[i].nums1, 0, tc[i].m, tc[i].nums2, 0, tc[i].n);
+        printArray(tc[i].nums1, tc[i].m + tc[i].n, "Output");
         printf("\n\n");
     }
 
