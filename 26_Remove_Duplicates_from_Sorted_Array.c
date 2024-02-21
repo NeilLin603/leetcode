794d6e9c4e185f652f1d0dce0ed775daa3c81f07
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-02-21 23:52:27 +0800

    Add files via upload

---
+++ 26_Remove_Duplicates_from_Sorted_Array.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int removeDuplicates(int *nums, int numsSize) {
+    int j = 0;
+    for (int i = 1; i < numsSize; i++) if (nums[i] != nums[j]) {
+        nums[++j] = nums[i];
+    }
+    return j + 1;
+}
+
+void printArray(const int *nums, int numsSize, const char *name) {
+    printf("%s = [", name);
+    if (numsSize) {
+        printf("%d", nums[0]);
+        for (int i = 1; i < numsSize; i++) {
+            printf(",%d", nums[i]);
+        }
+    }
+    printf("]\n");
+}
+
+int main() {
+    struct {
+        int nums[10];
+        int numsSize;
+    } tc[] = {
+        {.nums = {1,1,2}, .numsSize = 3},
+        {.nums = {0,0,1,1,1,2,2,3,3,4}, .numsSize = 10}};
+
+    for (int i = 0; i < sizeof(tc) / sizeof(tc[0]); i++) {
+        printf("Example %d:\nInput: ", i + 1);
+        printArray(tc[i].nums, tc[i].numsSize, "nums");
+        printf("Output: %d, ", removeDuplicates(tc[i].nums, tc[i].numsSize));
+        printArray(tc[i].nums, tc[i].numsSize, "nums");
+        printf("\n");
+    }
+
+    return 0;
+}
