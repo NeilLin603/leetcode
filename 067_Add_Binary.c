6548f4631384feb70c97d5939b41074e3a79a8c1
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-03-07 11:15:10 +0800

    Update 067_Add_Binary.c

--- 067_Add_Binary.c
+++ 067_Add_Binary.c
@@ -11,52 +11,29 @@ char *addBinary(char *a, char *b) {
     while (*++b) {
         bLen++;
     }
-    int strLen = MAX(aLen, bLen) + 1;
-    char *str = (char *)malloc((strLen + 1) * sizeof(char)), ovf = '0';
-    str[strLen--] = '\0';
+    int idx = MAX(aLen, bLen) + 1;
+    char *str = (char *)calloc((idx + 1), sizeof(char));
+    str[--idx] = '0';
     while (aLen && bLen) {
-        if (ovf == '1') {
-            str[strLen--] = *--a ^ *--b ? '0' : '1';
-            ovf = *a == '1' || *b == '1' ? '1' : '0';
+        if (str[idx] == '1') {
+            str[idx--] = *--a ^ *--b ? '0' : '1';
+            str[idx] = *a == '1' || *b == '1' ? '1' : '0';
         } else {
-            str[strLen--] = *--a ^ *--b ? '1' : '0';
-            ovf = *a & *b;
+            str[idx--] = *--a ^ *--b ? '1' : '0';
+            str[idx] = *a == '1' && *b == '1' ? '1' : '0';
         }
         aLen--;
         bLen--;
     }
     while (aLen--) {
-        if (ovf == '1') {
-            if (*--a == '1') {
-                str[strLen--] = '0';
-            } else {
-                str[strLen--] = '1';
-                ovf = '0';
-            }
-        } else {
-            str[strLen--] = *--a;
-        }
+        str[idx - 1] = *--a == '1' && str[idx] == '1' ? '1' : '0';
+        str[idx--] = str[idx] ^ *a ? '1' : '0';
     }
     while (bLen--) {
-        if (ovf == '1') {
-            if (*--b == '1') {
-                str[strLen--] = '0';
-            } else {
-                str[strLen--] = '1';
-                ovf = '0';
-            }
-        } else {
-            str[strLen--] = *--b;
-        }
+        str[idx - 1] = *--b == '1' && str[idx] == '1' ? '1' : '0';
+        str[idx--] = str[idx] ^ *b ? '1' : '0';
     }
-    if (ovf == '1') {
-        str[0] = '1';
-    } else {
-        do {
-            str[strLen] = str[strLen + 1];
-        } while (str[++strLen]);
-    }
-    return str;
+    return *str == '1' ? str : str + 1;
 }
 
 int main() {
@@ -65,7 +42,8 @@ int main() {
     } tc[] = {
         {.a = "11", .b = "1"},
         {.a = "1010", .b = "1011"},
-        {.a = "0", .b = "0"}};
+        {.a = "0", .b = "0"},
+        {.a = "100", .b = "110010"}};
     int tcSize = sizeof(tc) / sizeof(tc[0]);
     char *output;
 
@@ -74,7 +52,6 @@ int main() {
         printf("Input: a = \"%s\", b = \"%s\"\n", tc[i].a, tc[i].b);
         output = addBinary(tc[i].a, tc[i].b);
         printf("Output: %s\n\n", output);
-        free(output);
     }
 
     return 0;
