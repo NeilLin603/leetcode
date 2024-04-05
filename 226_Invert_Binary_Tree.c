9e24d46ae559c69b6c5b4a623da0135fc1ead241
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-04-05 18:04:35 +0800

    Create 226_Invert_Binary_Tree.c

---
+++ 226_Invert_Binary_Tree.c
@@ -0,0 +1,16 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode *invertTree(struct TreeNode *root) {
+    if (root) {
+        struct TreeNode *tmp = invertTree(root->left);
+        root->left = invertTree(root->right);
+        root->right = tmp;
+    }
+    return root;
+}
