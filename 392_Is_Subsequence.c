2cad0efff4bad78e29f0b2c4dc1a36842601a588
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-27 13:38:34 +0800

    Update 392_Is_Subsequence.c

--- 392_Is_Subsequence.c
+++ 392_Is_Subsequence.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 bool isSubsequence(char *s, char *t) {
     while (*s) {
@@ -14,21 +14,18 @@ bool isSubsequence(char *s, char *t) {
 }
 
 int main() {
-    struct TestCase {
+    struct {
         char *s, *t;
     } tc[] = {
         {.s = "abc", .t = "ahbgdc"},
-        {.s = "axc", .t = "ahbgdc"}};
+        {.s = "axc", .t = "ahbgdc"},
+        {.s = "", .t = "ahbgdc"}};
     int tcSize = sizeof(tc) / sizeof(tc[0]);
 
     for (int i = 0; i < tcSize; i++) {
         printf("Example %d:\n", i + 1);
         printf("Input: s = \"%s\", t = \"%s\"\n", tc[i].s, tc[i].t);
-        if (isSubsequence(tc[i].s, tc[i].t)) {
-            printf("Output: true\n\n");
-        } else {
-            printf("Output: false\n\n");
-        }
+        printf("Output: %s\n\n", isSubsequence(tc[i].s, tc[i].t) ? "true" : "false");
     }
 
     return 0;
