7f380e417cdd636578adb75e15cd0dd3fa0b5f95
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-28 10:51:40 +0800

    Update 055_Jump_Game.c

--- 055_Jump_Game.c
+++ 055_Jump_Game.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 bool canJump(int *nums, int numsSize) {
     for (int i = 0; i < numsSize - 1; i++) {
@@ -13,12 +13,12 @@ bool canJump(int *nums, int numsSize) {
     return true;
 }
 
-void printArray(const int *nums, int numsSize, const char *name) {
+void printArray(int *nums, int numsSize, char *name) {
     printf("%s = [", name);
     if (numsSize) {
-        printf("%d", nums[0]);
-        for (int i = 1; i < numsSize; i++) {
-            printf(",%d", nums[i]);
+        printf("%d", *nums);
+        while (--numsSize) {
+            printf(",%d", *++nums);
         }
     }
     printf("]\n");
@@ -35,11 +35,8 @@ int main() {
     for (int i = 0; i < tcSize; i++) {
         printf("Example %d:\n", i + 1);
         printArray(tc[i].nums, tc[i].numsSize, "Input: nums");
-        if (canJump(tc[i].nums, tc[i].numsSize)) {
-            printf("Output: true\n\n");
-        } else {
-            printf("Output: false\n\n");
-        }
+        printf("Output: %s\n\n",
+               canJump(tc[i].nums, tc[i].numsSize) ? "true" : "false");
     }
 
     return 0;
