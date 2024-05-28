4ce0bb75439d0218b0305c4d32322163306ce3c8
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-28 18:29:47 +0800

    Update 274_H-Index.c

--- 274_H-Index.c
+++ 274_H-Index.c
@@ -1,40 +1,43 @@
 #include <stdio.h>
 
-void quickSort(int *, int);
-
-int hIndex(int *citations, int citationsSize) {
-    quickSort(citations, citationsSize);
-    int i;
-    for (i = 0; i < citationsSize; i++) if (citations[i] < i + 1) {
-        break;
-    }
-    return i;
-}
+#define SWAP(a, b) do { \
+    (a) ^= (b); \
+    (b) ^= (a); \
+    (a) ^= (b); \
+} while (0)
 
 void quickSort(int *nums, int numsSize) {
     if (numsSize > 1) {
-        int s = nums[numsSize >> 1], i = -1, j = numsSize;
+        int i = -1, j = numsSize, s = nums[numsSize >> 1];
         for (;;) {
             while (nums[++i] > s);
             while (nums[--j] < s);
             if (i >= j) {
                 break;
             }
-            nums[i] ^= nums[j];
-            nums[j] ^= nums[i];
-            nums[i] ^= nums[j];
+            SWAP(nums[i], nums[j]);
         }
         quickSort(nums, i);
         quickSort(nums + j + 1, numsSize - j - 1);
     }
 }
 
-void printArray(const int *nums, int numsSize, const char *name) {
+int hIndex(int *citations, int citationsSize) {
+    quickSort(citations, citationsSize);
+    while (citationsSize--) {
+        if (citations[citationsSize] > citationsSize) {
+            break;
+        }
+    }
+    return citationsSize + 1;
+}
+
+void printArray(int *nums, int numsSize, char *name) {
     printf("%s = [", name);
     if (numsSize) {
-        printf("%d", nums[0]);
-        for (int i = 1; i < numsSize; i++) {
-            printf(",%d", nums[i]);
+        printf("%d", *nums);
+        while (--numsSize) {
+            printf(",%d", *++nums);
         }
     }
     printf("]\n");
@@ -45,9 +48,10 @@ int main() {
         int citations[5], citationsSize;
     } tc[] = {
         {.citations = {3,0,6,1,5}, .citationsSize = 5},
-        {.citations = {1,3,1}, .citationsSize = 3}};
+        {.citations = {1,3,1}, .citationsSize = 3}
+    };
     int tcSize = sizeof(tc) / sizeof(tc[0]);
-    
+
     for (int i = 0; i < tcSize; i++) {
         printf("Example %d:\n", i + 1);
         printArray(tc[i].citations, tc[i].citationsSize, "Input: citations");
