5bcd8ecd7c0321ece0aa4c539d1e3543bd292557
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 15:41:58 +0800

    Update 226_Invert_Binary_Tree.c

--- 226_Invert_Binary_Tree.c
+++ 226_Invert_Binary_Tree.c
@@ -1,3 +1,5 @@
+#include "binary_tree.h"
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -8,9 +10,39 @@
  */
 struct TreeNode *invertTree(struct TreeNode *root) {
     if (root) {
-        struct TreeNode *tmp = invertTree(root->left);
+        struct TreeNode *left = root->left;
         root->left = invertTree(root->right);
-        root->right = tmp;
+        root->right = invertTree(left);
     }
     return root;
+}
+
+int main() {
+    struct {
+        int vals[10], valsSize;
+    } tc[] = {
+        {.vals = {4,2,7,1,3,6,9}, .valsSize = 7},
+        {.vals = {2,1,3}, .valsSize = 3},
+        {.valsSize = 0}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *root;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        root = buildTree(tc[i].vals, tc[i].valsSize);
+        printTree(root, "Input: root");
+
+        // Output
+        root = invertTree(root);
+        printTree(root, "Output");
+        printf("\n");
+
+        // Free tree
+        freeTree(&root);
+    }
+
+    return 0;
 }
