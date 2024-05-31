91a232d71eef99af9b6a687b63656a1058dbf581
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 16:35:59 +0800

    Update 101_Symmetric_Tree.c

--- 101_Symmetric_Tree.c
+++ 101_Symmetric_Tree.c
@@ -1,16 +1,15 @@
-#include <stdbool.h>
+#include "binary_tree.h"
 
 /**
  * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
  */
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
-
 bool isSymmetricSub(struct TreeNode *l, struct TreeNode *r) {
-    if (!l && !r) {
+    if (!(l || r)) {
         return true;
     }
     if (!l && r || l && !r) {
@@ -19,12 +18,37 @@ bool isSymmetricSub(struct TreeNode *l, struct TreeNode *r) {
     if (l->val != r->val) {
         return false;
     }
-    return isSymmetricSub(l->left, r->right) && isSymmetricSub(l->right, r->left);
+    return isSymmetricSub(l->left, r->right) &&
+           isSymmetricSub(l->right, r->left);
 }
 
 bool isSymmetric(struct TreeNode *root) {
-    if (root) {
-        return isSymmetricSub(root->left, root->right);
+    return isSymmetricSub(root->left, root->right);
+}
+
+int main() {
+    struct {
+        int vals[10], valsSize;
+    } tc[] = {
+        {.vals = {1,2,2,3,4,4,3}, .valsSize = 7},
+        {.vals = {1,2,2,NULL_NODE,3,NULL_NODE,3}, .valsSize = 7}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *root;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        root = buildTree(tc[i].vals, tc[i].valsSize);
+        printTree(root, "Input: root");
+
+        // Output
+        printf("Output: %s\n\n", isSymmetric(root) ? "true" : "false");
+
+        // Free tree
+        freeTree(&root);
     }
-    return true;
+
+    return 0;
 }
