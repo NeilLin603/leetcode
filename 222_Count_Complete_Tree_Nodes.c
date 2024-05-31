b34b127dce36b80b368dc43b4d46fde2324a0e25
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 18:04:32 +0800

    Update 222_Count_Complete_Tree_Nodes.c

--- 222_Count_Complete_Tree_Nodes.c
+++ 222_Count_Complete_Tree_Nodes.c
@@ -1,12 +1,41 @@
+#include "binary_tree.h"
+
 /**
  * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
  */
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
-
 int countNodes(struct TreeNode *root) {
     return root ? 1 + countNodes(root->left) + countNodes(root->right) : 0;
+}
+
+int main() {
+    struct {
+        int vals[16], valsSize;
+    } tc[] = {
+        {.vals = {1,2,3,4,5,6}, .valsSize = 6},
+        {.valsSize = 0},
+        {.vals = {1}, .valsSize = 1}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *root;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        root = buildTree(tc[i].vals, tc[i].valsSize);
+        printTree(root, "Input: root");
+
+        // Output
+        printf("\nOutput: %d:\n\n", countNodes(root));
+
+        // Free tree
+        freeTree(&root);
+    }
+
+    return 0;
 }
