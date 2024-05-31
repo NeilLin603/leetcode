ae19f0f52c78e7450446f0f1072d3fe44d033ea3
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 15:11:47 +0800

    Update 100_Same_Tree.c

--- 100_Same_Tree.c
+++ 100_Same_Tree.c
@@ -1,3 +1,5 @@
+#include "binary_tree.h"
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -7,14 +9,44 @@
  * };
  */
 bool isSameTree(struct TreeNode *p, struct TreeNode *q) {
-    if (!p && !q) {
+    if (!(p || q)) {
         return true;
     }
-    if (p && !q || !p && q) {
+    if (!p && q || p && !q) {
         return false;
     }
     if (p->val != q->val) {
         return false;
     }
     return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
+
+int main() {
+    struct {
+        int pVals[10], pSize, qVals[10], qSize;
+    } tc[] = {
+        {.pVals = {1, 2, 3}, .pSize = 3, .qVals = {1, 2, 3}, .qSize = 3},
+        {.pVals = {1, 2}, .pSize = 2, .qVals = {1, NULL_NODE, 2}, .qSize = 3}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *p, *q;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        p = buildTree(tc[i].pVals, tc[i].pSize);
+        printTree(p, "p");
+        q = buildTree(tc[i].qVals, tc[i].qSize);
+        printTree(q, "q");
+
+        // Output
+        printf("Output: %s\n\n", isSameTree(p, q) ? "true" : "false");
+
+        // Free trees
+        freeTree(&p);
+        freeTree(&q);
+    }
+
+    return 0;
 }
