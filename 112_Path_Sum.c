d2edaf5a29f345e370dec3320e95cf11c3999e8c
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 17:54:46 +0800

    Update 112_Path_Sum.c

--- 112_Path_Sum.c
+++ 112_Path_Sum.c
@@ -1,30 +1,51 @@
-#include <stdbool.h>
+#include "binary_tree.h"
 
 /**
  * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
  */
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
-
-bool hasPathSumSub(struct TreeNode *root, int targetSum) {
-    if (!root->left && !root->right) {
-        return root->val == targetSum;
-    }
-    if (root->left) if (hasPathSumSub(root->left, targetSum - root->val)) {
-        return true;
-    }
-    if (root->right) if (hasPathSumSub(root->right, targetSum - root->val)) {
-        return true;
-    }
-    return false;
-}
-
 bool hasPathSum(struct TreeNode *root, int targetSum) {
     if (!root) {
         return false;
     }
-    return hasPathSumSub(root, targetSum);
+    targetSum -= root->val;
+    if (!(root->left || root->right)) {
+        return !targetSum;
+    }
+    return hasPathSum(root->left, targetSum) ||
+           hasPathSum(root->right, targetSum);
+}
+
+int main() {
+    struct {
+        int vals[16], valsSize, targetSum;
+    } tc[] = {
+        {.vals = {5,4,8,11,NULL_NODE,13,4,7,2,NULL_NODE,NULL_NODE,NULL_NODE,1},
+         .valsSize = 13, .targetSum = 22},
+        {.vals = {1,2,3}, .valsSize = 3, .targetSum = 5}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *root;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        root = buildTree(tc[i].vals, tc[i].valsSize);
+        printTree(root, "Input: root");
+        printf(", targetSum = %d", tc[i].targetSum);
+
+        // Output
+        printf("\nOutput: %s\n\n",
+               hasPathSum(root, tc[i].targetSum) ? "true" : "false");
+
+        // Free tree
+        freeTree(&root);
+    }
+
+    return 0;
 }
