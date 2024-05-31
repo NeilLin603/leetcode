144dadeabde0899397e896d760286fdd6ee83a3c
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-05-31 14:52:39 +0800

    Update 104_Maximum_Depth_of_Binary_Tree.c

--- 104_Maximum_Depth_of_Binary_Tree.c
+++ 104_Maximum_Depth_of_Binary_Tree.c
@@ -1,14 +1,46 @@
+#include "binary_tree.h"
+
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 /**
  * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
  */
-struct TreeNode {
-    int val;
-    struct TreeNode *left;
-    struct TreeNode *right;
-};
-
 int maxDepth(struct TreeNode *root) {
-    return root ? 1 + MAX(maxDepth(root->left), maxDepth(root->right)) : 0;
+    if (!root) {
+        return 0;
+    }
+    int left = maxDepth(root->left), right = maxDepth(root->right);
+    return 1 + MAX(left, right);
+}
+
+int main() {
+    struct {
+        int vals[10], valsSize;
+    } tc[] = {
+        {.vals = {3, 9, 20, NULL_NODE, NULL_NODE, 15, 7}, .valsSize = 7},
+        {.vals = {1, NULL_NODE, 2}, .valsSize = 3}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
+
+    Node_t *root;
+    for (int i = 0; i < tcSize; i++) {
+        printf("Example %d:\n", i + 1);
+
+        // Input
+        root = createTree(tc[i].vals, tc[i].valsSize);
+        printTree(root, "Input: root");
+
+        // Output
+        printf("Output: %d\n\n", maxDepth(root));
+
+        // Free tree
+        freeTree(&root);
+    }
+
+    return 0;
 }
