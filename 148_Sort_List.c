3ece9c62e777da9ddbc6c5b74a2ec62e4493ce4b
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-06-03 17:08:03 +0800

    Update 148_Sort_List.c

--- 148_Sort_List.c
+++ 148_Sort_List.c
@@ -1,82 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define SWAP(a, b) do { \
-    (a) ^= (b); \
-    (b) ^= (a); \
-    (a) ^= (b); \
-} while (0)
+#include "linked_list_basic.h"
 
 /**
  * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
  */
-typedef struct ListNode {
-    int val;
-    struct ListNode *next;
-} Node_t;
-
-struct ListNode* sortList(struct ListNode* head) {
-    if (head) {
-        struct ListNode *cur = head->next;
-        while (cur) {
-            if (cur->val < head->val) {
-                SWAP(cur->val, head->val);
-            }
-            cur = cur->next;
-        }
-        sortList(head->next);
+struct ListNode *merge(struct ListNode *list1, struct ListNode *list2) {
+    if (!list1) {
+        return list2;
     }
-    return head;
+    if (!list2) {
+        return list1;
+    }
+    if (list1->val < list2->val) {
+        list1->next = merge(list1->next, list2);
+        return list1;
+    }
+    list2->next = merge(list1, list2->next);
+    return list2;
 }
 
-Node_t *createList(int *nums, int numsSize) {
-    if (!numsSize) {
-        return NULL;
+struct ListNode *sortList(struct ListNode *head) {
+    if (!head || !head->next) {
+        return head;
     }
-    Node_t *head = (Node_t *)malloc(sizeof(Node_t));
-    head->val = *nums;
-    head->next = createList(nums + 1, numsSize - 1);
-    return head;
-}
-
-void printList(Node_t *head, char *name) {
-    printf("%s = ", name);
-    while (head) {
-        printf("(%d)->", head->val);
-        head = head->next;
+    struct ListNode *slow = head, *fast = head->next;
+    while (fast->next && fast->next->next) {
+        slow = slow->next;
+        fast = fast->next->next;
     }
-    printf("null\n");
-}
-
-void freeList(Node_t **head) {
-    Node_t *tmp;
-    while (*head) {
-        tmp = *head;
-        *head = tmp->next;
-        free(tmp);
-    }
+    fast = slow->next;
+    slow->next = NULL;
+    head = sortList(head);
+    fast = sortList(fast);
+    return merge(head, fast);
 }
 
 int main() {
     struct {
-        int nums[5], numsSize;
+        int nums[16], numsSize;
     } tc[] = {
         {.nums = {4,2,1,3}, .numsSize = 4},
         {.nums = {-1,5,3,4,0}, .numsSize = 5},
-        {.numsSize = 0}};
-    int tcSize = sizeof(tc) / sizeof(tc[0]);
+        {.numsSize = 0}
+    };
+    int tcSize = sizeof(tc) / sizeof(*tc);
 
     Node_t *head;
     for (int i = 0; i < tcSize; i++) {
         printf("Example %d:\n", i + 1);
 
+        // Input
         head = createList(tc[i].nums, tc[i].numsSize);
         printList(head, "Input: head");
 
+        // Output
         head = sortList(head);
-        printList(head, "Output");
-
+        printList(head, "\nOutput");
         printf("\n");
+
         freeList(&head);
     }
 
