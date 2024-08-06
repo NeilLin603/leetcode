e8fa85b85d8614b0f28fafe952ff3ab852b6f0ff
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-08-06 09:12:38 +0800

    Update 190_Reverse_Bits.c

--- 190_Reverse_Bits.c
+++ 190_Reverse_Bits.c
@@ -7,7 +7,7 @@
 uint32_t reverseBits(uint32_t n) {
     uint32_t m = 0;
     for (int i = 0; i < 32; i++) {
-        m |= (n & 1) << 31 - i;
+        m = m << 1 | n & 1;
         n >>= 1;
     }
     return m;
